a529fae17f4cf3fcbaecce2544b6f99c6044b4db
hellotutorialhi <38435384+hellotutorialhi@users.noreply.github.com>
2018-04-16 21:58:45 +0530

    Add files via upload

---
+++ src/com/test/Person2.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Person2 extends Person{
+public void talk(){
+	System.out.println("Person2");
+}
+}
---
+++ src/com/test/Test.java
@@ -0,0 +1,21 @@
+package com.test;
+
+// Java program to count number of 
+// possible ways to a given score
+// can be reached in a game where 
+// a move can earn 3 or 5 or 10
+import java.util.Arrays;
+
+public class Test
+{
+	public static void main(String args[]){
+		Person p = new Person2();
+		p.talk();
+		
+		Double x = 3.14;
+		int y = (int)Math.abs(x);
+		System.out.println(y);
+	}
+}
+
+// This code is contributed by Anant Agarwal.
